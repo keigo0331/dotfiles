70822bae7835a2b9e010a554e3c6b73e1d69f775
keigo0331 <k.kamiyama3@gmail.com>
2022-07-08 00:46:01 +0900

    シンボリックリンクを貼るスクリプトファイルを作成

---
+++ symbolic_links.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+DOTFILES_PATH=$HOME/dev/dotfiles/
+
+ln -sf $DOTFILES_PATH/zsh/.zshrc ~/.zshrc
+ln -sf $DOTFILES_PATH/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $DOTFILES_PATH/git/.gitconfig ~/.gitconfig
+
+mkdir -p $HOME/.config/nvim/plugins $HOME/.config/nvim/undo
+
+NVIM_FILES=$(ls -aF $DOTFILES_PATH/nvim/ | grep -v /)
+
+for file in ${NVIM_FILES[@]}
+do
+  ln -sf $DOTFILES_PATH/nvim/$file ~/.config/nvim/$file
+done
+
+NVIM_PLUGIN_FILES=$(ls -aF $DOTFILES_PATH/nvim/plugins | grep -v /)
+
+for file in ${NVIM_PLUGIN_FILES[@]}
+do
+  ln -sf $DOTFILES_PATH/nvim/plugins/$file ~/.config/nvim/plugins/$file
+done
